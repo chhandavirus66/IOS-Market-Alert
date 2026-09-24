ec4256d110d66681addc5a45dd05c7cc5eb48a58
chhandavirus66 <chhandavirus@gmail.com>
2026-09-24 14:52:11 +0530

    Update instruments.py

--- instruments.py
+++ instruments.py
@@ -1,5 +1,5 @@
 MARKET_INSTRUMENTS = {
-    "RELIANCE": {"target_price": 2500},
-    "TCS": {"target_price": 3800},
-    "HDFCBANK": {"target_price": 1600}
+    "RELIANCE.NS": {"target_price": 2500},
+    "TCS.NS": {"target_price": 3800},
+    "HDFCBANK.NS": {"target_price": 1600}
 }
